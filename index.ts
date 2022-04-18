6a9300a4ad0adea6e3841a4ce7587a8d219e5466
Kalixer <al.caceres.ag72@gmail.com>
2022-04-18 16:04:27 -0400

    Proyect finished

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 const options: string[] = [];
 
+let elegido: boolean = false;
+
+
 
 const read: HTMLElement = document.getElementById("read");
 read.addEventListener("click", pushRead)
@@ -9,27 +12,110 @@ const study: HTMLElement = document.getElementById("study");
 study.addEventListener("click", pushStudy)
 const kondo: HTMLElement = document.getElementById("kondo");
 kondo.addEventListener("click", pushKondo)
+const anime: HTMLElement = document.getElementById("anime");
+anime.addEventListener("click", pushAnime)
+
+const main: HTMLElement = document.getElementById("main")
+
+
+const spiner: HTMLElement = document.getElementById("spin-container")
+// spiner.addEventListener('click', spin)
+const spiner_button: HTMLElement = document.getElementById("spinner")
+spiner_button.addEventListener('click', spin)
+
+
 
 function pushRead() {
     options.push('Read');
-    console.log(options)
+    read.style.backgroundColor = "green";
+
+    elegido = true;
 }
 function pushDraw() {
     options.push('Draw');
-    console.log(options)
+    draw.style.backgroundColor = "green";
+
+    elegido = true;
 }
 function pushStudy() {
-    options.push('Study');
-    console.log(options)
+    options.push('Study')
+    study.style.backgroundColor = "green";
+    
+    elegido = true;
 }
 function pushKondo() {
-    options.push('Kondo');
-    console.log(options)
+    options.push('Kondo')
+    kondo.style.backgroundColor = "green";
+
+    elegido = true;
+}
+function pushAnime() {
+    options.push('Anime')
+    anime.style.backgroundColor = "green";
+
+    elegido = true;
 }
 
-const spiner: HTMLElement = document.getElementById("spinner")
-spiner.addEventListener('click', spin)
+
 
 function spin() {
-    console.log(options)
+    if (elegido) {
+        
+        let num_aleatorio = aleatorio(0, options.length - 1)
+        console.log(options)
+        console.log(num_aleatorio)
+
+        main.style.display = "flex";
+        main.style.justifyContent = "center"
+
+        main.innerHTML = `
+        <div class="loader-wrapper" id="loader-w">
+            <div class="loader">
+                <div class="loader loader-inner"></div>
+            </div>
+        </div>
+        `
+    
+        setTimeout(() => {
+            main.innerHTML = `
+            <div class="box" id="carga">${options   [num_aleatorio]}
+            </div>
+            `
+        }, 3000)
+
+        function aleatorio(min:number, max:number):number {
+            const numero_r: number = Math.floor(Math.random() * (max - min + 1)) + min;
+        
+            console.log(numero_r)
+            return numero_r
+        }
+        
+        
+    } else {
+        const message:string = ":|"
+        console.log("Nani!")
+        
+        main.style.display = "flex";
+        main.style.justifyContent = "center"
+
+        main.innerHTML = `
+        <div class="loader-wrapper" id="loader-w">
+            <div class="loader">
+                <div class="loader loader-inner"></div>
+            </div>
+        </div>
+        `
+    
+        setTimeout(() => {
+            main.innerHTML = `
+            <div class="box" id="carga">${message}
+            </div>
+            `
+        }, 3000)
+    }
 }
+
+
+
+
+
